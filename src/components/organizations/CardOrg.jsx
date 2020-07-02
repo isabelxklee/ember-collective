8c340cba9d096babea1144e4215e8eb5d2bc6d45
isabel k. lee <hello@isabelklee.co>
2020-07-02 17:14:47 -0400

    updated card CSS

--- src/components/organizations/CardOrg.jsx
+++ src/components/organizations/CardOrg.jsx
@@ -48,22 +48,27 @@ class CardOrg extends Component {
   render() {
     let {id, name, website, location, tagline, donation_link} = this.props.org
     let orgsTags = this.findOrgsTags().map((tag) => {
+      // return <Tag key={tag.id} tag={tag} />
       return `#${tag.content} `
     })
 
     return (
       <div className="org-tile">
+        <div className="card-header">
         <Link to={`/organizations/${id}`}>
           <h4 className="org-tile-title">{location}</h4>
           <h2 className="org-tile-title">{name}</h2>
-          <p className="org-tile-title">{this.truncateString(tagline, 150)}</p>
-        </Link>
-      
-        <div className="btn-group">
-          <button className="small-button"><a href={website} target="blank" className="small-button">Website</a></button>
-          <button className="small-button"><a href={donation_link} target="blank" className="small-button">Donate</a></button>
+          </Link>
         </div>
-        {/* {orgsTags} */}
+
+        <div className="card-body">
+          <p className="org-tile-title">{this.truncateString(tagline, 200)}</p>
+          <div className="btn-group">
+            <button className="card-button"><a href={website} target="blank" className="card-button">Website</a></button>
+            <button className="card-button"><a href={donation_link} target="blank" className="card-button">Donate</a></button>
+          </div>
+          {/* {orgsTags} */}
+        </div>
       </div>
     )
   }
