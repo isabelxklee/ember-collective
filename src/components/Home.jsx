0b48d1922be58f3c88b5d827df3b30133e3ec0d8
isabel k. lee <hello@isabelklee.co>
2020-07-17 00:08:44 -0400

    updated components in Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
 import FilterOrgs from './organizations/FilterOrgs.jsx'
 import OrgContainer from '../components/organizations/OrgContainer'
 
