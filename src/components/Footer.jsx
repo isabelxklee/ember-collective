d7abfa8cf80c54db1c9d0f4074a40bf0eb611d64
isabel k. lee <hello@isabelklee.co>
2020-08-04 13:39:43 -0400

    updated footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,41 @@
-import React, { Component } from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
 
-class Footer extends Component {
-  render() {
-    return (
-      <div className="footer">
-        <h5><a href="https://github.com/isabelxklee/ember-collective" target="blank" className="footer-left">Made with <span role="img" aria-label="heart-white">🤍</span>by Isabel K. Lee</a></h5>
-        <h5> <Link to="/about" className="footer-right">About Ember Collective <span role="img" aria-label="flame">🔥</span></Link></h5>
-      </div>
-    )
-  }
+function Footer() {
+  const [windowDimension, setWindowDimension] = useState(null)
+
+  useEffect(() => {
+    setWindowDimension(window.innerWidth)
+  }, [])
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimension(window.innerWidth)
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [])
+
+  const isMobile = windowDimension <= 892
+
+  return(
+    <div className="footer">
+
+      { isMobile ? 
+        <div className="btn-group">
+          <button className="footer-button"><Link to="/about" className="card"><span role="img" aria-label="flame">🔥</span> About Ember Collective</Link></button>
+          <button className="footer-button"><a href="https://github.com/isabelxklee/ember-collective" target="blank" className="card"><span role="img" aria-label="white heart">🤍</span> GitHub</a></button>
+        </div>
+      :
+      <>
+        <h5> <Link to="/about" className="footer-left"><span role="img" aria-label="flame">🔥</span> About Ember Collective</Link></h5>
+        <h5><a href="https://github.com/isabelxklee/ember-collective" target="blank" className="footer-right"><span role="img" aria-label="white heart">🤍</span> GitHub</a></h5>
+        </>
+      }
+
+  </div>
+  )
 }
 
 export default Footer
