a0bea0964e12a8e9af5f2861fc4505903a43ccfb
isabel k. lee <hello@isabelklee.co>
2020-09-14 15:11:22 -0400

    udpated async process

--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ import { usePromiseTracker } from "react-promise-tracker"
 const LoadingIndicator = props => {
   const { promiseInProgress } = usePromiseTracker()
   return (
-    promiseInProgress && 
-    <h1>Hey some async call in progress ! </h1>
+    promiseInProgress
   )
 }
 
